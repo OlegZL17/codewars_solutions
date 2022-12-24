0eb168d70a0e877b7f2130128e157fe18d808f17
Oleg <golos.donbassa25@gmail.com>
2022-12-24 05:27:16 +0200

    new solutions

--- 4_kyu/Codewars style ranking system.py
+++ 4_kyu/Codewars style ranking system.py
@@ -31,7 +31,8 @@ Logic Examples:
 If a user ranked -8 completes an activity ranked -7 they will receive 10 progress
 If a user ranked -8 completes an activity ranked -6 they will receive 40 progress
 If a user ranked -8 completes an activity ranked -5 they will receive 90 progress
-If a user ranked -8 completes an activity ranked -4 they will receive 160 progress, resulting in the user being upgraded to rank -7 and having earned 60 progress towards their next rank
+If a user ranked -8 completes an activity ranked -4 they will receive 160 progress,
+ resulting in the user being upgraded to rank -7 and having earned 60 progress towards their next rank
 If a user ranked -1 completes an activity ranked 1 they will receive 10 progress (remember, zero rank is ignored)
 Code Usage Examples:
 user = User()
